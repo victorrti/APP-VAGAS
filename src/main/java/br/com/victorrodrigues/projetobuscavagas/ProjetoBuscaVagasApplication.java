c665900945c425ff0121b724d7ec1ce447e58c95
victorrti <victorrodriguesti@gmail.com>
2024-03-05 22:14:16 -0300

    first commit

---
+++ src/main/java/br/com/victorrodrigues/projetobuscavagas/ProjetoBuscaVagasApplication.java
@@ -0,0 +1,14 @@
+package br.com.victorrodrigues.projetobuscavagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBuscaVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBuscaVagasApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/br/com/victorrodrigues/projetobuscavagas/ProjetoBuscaVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorrodrigues.projetobuscavagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBuscaVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

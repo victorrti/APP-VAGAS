06b6199f365cfb8b3c6832edcbca29f31380dc82
victorrti <victorrodriguesti@gmail.com>
2024-03-14 21:13:45 -0300

    configurando banco de dados e a classe de canditado

--- src/main/java/br/com/victorrodrigues/projetobuscavagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/victorrodrigues/projetobuscavagas/modules/candidate/CandidateEntity.java
@@ -1,14 +1,24 @@
 package br.com.victorrodrigues.projetobuscavagas.modules.candidate;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name ="candidate")
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID )
     private String UUID;
     private String name;
     @NotBlank()
@@ -20,5 +30,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculo;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     
 }
